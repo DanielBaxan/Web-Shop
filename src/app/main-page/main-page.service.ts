5da1740f175b46dbf624407c2242b9f3a190ca96
BaxLi <lilian.baxan@gmail.com>
2021-02-07 19:56:42 +0200

    ok2

---
+++ src/app/main-page/main-page.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Observable } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainPageService {
+  private baseUrl = 'http://localhost:5000/api/'
+  constructor(private http: HttpClient) {}
+
+  getApi(addToUrl: string): Observable<any> {
+    let params = new HttpParams()
+    return this.http.get<any>(`${this.baseUrl}${addToUrl}`, { params })
+  }
+}
