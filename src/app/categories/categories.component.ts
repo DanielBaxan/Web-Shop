7bb7f4812c39aed061e83783facbb9ad36df7765
DanielBaxan <daniel.baxan@gmail.com>
2021-02-17 12:48:48 +0200

    Added category routes

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy } from '@angular/core'
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router'
 
 const sortingOptions = [
   'popularitate',
@@ -12,10 +13,44 @@ const sortingOptions = [
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnDestroy {
   defaultSort = 'Sortează după cele mai recente'
   maxResults = 373
   currentMaxResults = 12
   sortings = sortingOptions
-  constructor() {}
+  path = ''
+  navigationSubscription
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+    // subscribe to the router events. Store the subscription so we can
+    // unsubscribe later.
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {
+      // If it is a NavigationEnd event re-initalise the component
+      if (e instanceof NavigationEnd) {
+        this.resetData()
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe()
+    }
+  }
+
+  resetData() {
+    this.path = 'prima pagină'
+    this.activatedRoute.params.subscribe(params => {
+      let mainCategory = params['mainCategory']
+      let category = params['category']
+      let subCategory = params['subCategory']
+
+      this.path = this.path + ` / ${mainCategory}`
+      if (category !== undefined) {
+        this.path = this.path + ` / ${category}`
+      }
+      if (subCategory !== undefined) {
+        this.path = this.path + ` / ${subCategory}`
+      }
+    })
+  }
 }
--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -6,19 +6,15 @@ import { HeaderModule } from '../shared/header/header.module'
 import { FooterModule } from '../shared/footer/footer.module'
 import { MaterialComponentsModule } from '../shared/material.module'
 import { FilterComponent } from './filter/filter.component'
-import { NgxSliderModule } from '@angular-slider/ngx-slider';
+import { NgxSliderModule } from '@angular-slider/ngx-slider'
 import { SideNavComponent } from './side-nav/side-nav.component'
+import { categoriesRoutes } from './categories.routes'
 
 @NgModule({
   declarations: [CategoriesComponent, FilterComponent, SideNavComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: CategoriesComponent
-      }
-    ]),
+    RouterModule.forChild(categoriesRoutes),
     HeaderModule,
     FooterModule,
     MaterialComponentsModule,
