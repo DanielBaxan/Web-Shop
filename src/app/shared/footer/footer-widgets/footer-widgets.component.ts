04a48d8d2a4766394f0d3bb993301f83e8a8f8ec
DanielBaxan <daniel.baxan@gmail.com>
2021-03-01 13:08:36 +0200

    Footer db items

--- src/app/shared/footer/footer-widgets/footer-widgets.component.ts
+++ src/app/shared/footer/footer-widgets/footer-widgets.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core'
+import { MainPageService } from 'src/app/main-page/main-page.service'
+import { ItemModel } from '../../constants'
 
 @Component({
   selector: 'app-footer-widgets',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core'
   styleUrls: ['./footer-widgets.component.scss']
 })
 export class FooterWidgetsComponent {
-  constructor() {}
+  items: Array<ItemModel> = []
+  shownItems: Array<ItemModel> = []
+  shownItemsIdx = 0
+
+  constructor(private mainPageService: MainPageService) {
+    this.mainPageService.getApi('items/getAllItems').subscribe(data => {
+      data.forEach((item: ItemModel) => this.items.push(item))
+
+      // TESTING BLOCK
+      this.items.push(data[1])
+      this.items.push(data[3])
+      this.items.push(data[1])
+      this.items.push(data[5])
+      this.items.push(data[4])
+      this.items.push(data[2])
+      this.items.push(data[4])
+      this.items.push(data[5])
+      this.items.push(data[2])
+      this.items.push(data[3])
+      this.items.push(data[0])
+      this.items.push(data[1])
+      this.items.push(data[5])
+      this.items.push(data[0])
+      this.items.push(data[3])
+      this.items.push(data[4])
+      this.items.push(data[5])
+
+      for (this.shownItemsIdx; this.shownItemsIdx < 4; this.shownItemsIdx++) {
+        this.shownItems.push(this.items[this.shownItemsIdx])
+      }
+    })
+  }
 }
