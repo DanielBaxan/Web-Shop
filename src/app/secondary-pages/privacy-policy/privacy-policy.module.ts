1d587c0a35ecc2ef5bd2429028b80c5717c92495
Dencikus <denis.ivlentievv@gmail.com>
2021-10-01 02:27:13 +0300

    Private-Done

--- src/app/secondary-pages/privacy-policy/privacy-policy.module.ts
+++ src/app/secondary-pages/privacy-policy/privacy-policy.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common'
 import { PrivacyPolicyComponent } from './privacy-policy.component'
 import { SharedModule } from 'src/app/shared/shared.module'
 import { RouterModule } from '@angular/router'
+import { HeaderModule } from 'src/app/shared/header/header.module'
+import { FooterModule } from 'src/app/shared/footer/footer.module'
+
 
 @NgModule({
   declarations: [PrivacyPolicyComponent],
   imports: [
     CommonModule,
     SharedModule,
+    HeaderModule,
+    FooterModule,
     RouterModule.forChild([
       {
         path: '',
