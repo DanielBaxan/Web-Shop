206ab63901d499d0622c55bb4e1dd66c7139d4c8
Dencikus <denis.ivlentievv@gmail.com>
2021-10-01 03:10:24 +0300

    How to - Done

--- src/app/secondary-pages/how-to-order/how-to-order.module.ts
+++ src/app/secondary-pages/how-to-order/how-to-order.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common'
 import { HowToOrderComponent } from './how-to-order.component'
 import { SharedModule } from 'src/app/shared/shared.module'
 import { RouterModule } from '@angular/router'
+import { HeaderModule } from 'src/app/shared/header/header.module'
+import { FooterModule } from 'src/app/shared/footer/footer.module'
 
 @NgModule({
   declarations: [HowToOrderComponent],
   imports: [
     CommonModule,
     SharedModule,
+    HeaderModule,
+    FooterModule,
     RouterModule.forChild([
       {
         path: '',
