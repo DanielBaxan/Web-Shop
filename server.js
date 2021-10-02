5c4aa88b6a8e5d8995a0cfd5ad042e20cf4f7044
DanielBaxan <daniel.baxan@gmail.com>
2021-10-02 17:07:26 +0200

    Heroku build config

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express()
 const port = process.env.PORT || 8080
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/webshop-test-daniel'))
+app.use(express.static(__dirname + '/dist/Web-Shop'))
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/webshop-test-daniel/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/Web-Shop/index.html'))
 })
 
 // Start the app by listening on the default Heroku port
